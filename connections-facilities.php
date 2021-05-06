bd92dab8b19b3fefcac79e242cf99cc6f9a1f284
Steven A. Zahm <shazahm1@hotmail.com>
2021-05-06 13:26:13 -0400

    Correct code alignment.

--- connections-facilities.php
+++ connections-facilities.php
@@ -738,21 +738,21 @@ HEREDOC;
 	 */
 	function Connections_Facilities() {
 
-			if ( class_exists('connectionsLoad') ) {
+		if ( class_exists( 'connectionsLoad' ) ) {
 
-					return new Connections_Facilities();
+			return new Connections_Facilities();
 
-			} else {
+		} else {
 
-				add_action(
-					'admin_notices',
-					function() {
-						echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Facilities.</p></div>';
-					}
-				);
+			add_action(
+				'admin_notices',
+				function() {
+					echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Facilities.</p></div>';
+				}
+			);
 
-				return FALSE;
-			}
+			return false;
+		}
 	}
 
 	/**
